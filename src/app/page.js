ad89d6d8dba9d547194f07126cda3f749c5d872b
Joseph Attia <josephattia159@gmail.com>
2024-05-13 13:26:10 -0400

    shrunk text

--- src/app/page.js
+++ src/app/page.js
@@ -96,7 +96,7 @@ export default function Home() {
       <div className="bg-base-200 py-10">
         <h2 className="text-5xl font-bold text-center mb-6 mt-10">Students lack enough practice problems</h2>
         <div className="flex justify-center items-center m-10 text-whiteWhite rounded-lg ">
-          <p className="text-center text-5xl bg-darkBlue w-1/2 p-4 md:text-3xl">Hindering their Learning</p>
+          <p className="text-center text-3xl bg-darkBlue w-1/2 p-4 md:text-3xl">Hindering their Learning</p>
         </div>
 
 
